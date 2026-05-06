393c0dceef30b3d7843e4019505a1bd3cd9ce2d2
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-06 23:44:28 +0300

    Add solution for Palindrome Number

--- LeetCode/Palindrome-Number/Solution.java
+++ LeetCode/Palindrome-Number/Solution.java
@@ -1,15 +1,18 @@
-1// UPDATE
-2class Solution {
-3    public boolean isPalindrome(int x) {
-4        
-5        String s = Integer.toString(x);
-6        String ters = new StringBuilder(s).reverse().toString();
-7
-8        if(x < 0) {
-9            return false;
-10        }
-11        else{
-12            return s.equals(ters);
-13        }
-14    }
-15}
+1class Solution {
+2    public boolean isPalindrome(int x) {
+3        if (x < 0) {
+4            return false;
+5        }
+6
+7        int n = x;
+8        int revnum = 0;
+9
+10        while (n > 0) {
+11            int d = n % 10;
+12            revnum = revnum * 10 + d;
+13            n = n / 10;
+14        }
+15
+16        return revnum == x;
+17    }
+18}
