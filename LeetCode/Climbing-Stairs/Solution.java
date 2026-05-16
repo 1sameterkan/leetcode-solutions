5b82d1b0d79e789dc93563d68c07b0b1ea1eb082
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-16 17:08:55 +0300

    Update Solution.java

--- LeetCode/Climbing-Stairs/Solution.java
+++ LeetCode/Climbing-Stairs/Solution.java
@@ -1,18 +1,18 @@
-1class Solution {
-2    public int climbStairs(int n) {
-3        
-4        if(n == 1){
-5            return 1;
-6        }
-7
-8        int[] fib = new int[n + 1];
-9        fib[1] = 1;
-10        fib[2] = 2;
-11
-12        for(int i = 3; i <= n; i++){
-13            fib[i] = fib[i - 1] + fib[i - 2];
-14        }
-15
-16        return fib[n];
-17    }
-18}
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n == 1){
+            return 1;
+        }
+      
+        int[] fib = new int[n + 1];
+        fib[1] = 1;
+        fib[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+      
+        return fib[n];
+    }
+}
