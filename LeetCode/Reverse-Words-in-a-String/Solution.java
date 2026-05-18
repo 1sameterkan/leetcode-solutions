663447e326bfb22abcfb3abb92d621f9892e8aa0
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-19 02:13:57 +0300

    Update Solution.java

--- LeetCode/Reverse-Words-in-a-String/Solution.java
+++ LeetCode/Reverse-Words-in-a-String/Solution.java
@@ -1,19 +1,19 @@
-1class Solution {
-2    public String reverseWords(String s) {
-3        String[] kelimeler = s.trim().split("\\s+");
-4
-5        StringBuilder tersCumle = new StringBuilder();
-6
-7        int i = kelimeler.length - 1;
-8
-9        while(i >= 0){
-10            tersCumle.append(kelimeler[i]);
-11            if(i > 0){
-12                tersCumle.append(" ");
-13            }
-14            i--;
-15        }
-16
-17        return tersCumle.toString();
-18    }
-19}
+class Solution {
+    public String reverseWords(String s) {
+        String[] kelimeler = s.trim().split("\\s+");
+
+        StringBuilder tersCumle = new StringBuilder();
+
+        int i = kelimeler.length - 1;
+
+        while(i >= 0){
+            tersCumle.append(kelimeler[i]);
+            if(i > 0){
+                tersCumle.append(" ");
+            }
+            i--;
+        }
+
+        return tersCumle.toString();
+    }
+}
