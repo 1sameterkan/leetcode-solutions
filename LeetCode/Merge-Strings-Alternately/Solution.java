a23ef84cf22860dcd59139ffb254e61e9eb87435
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-18 17:14:33 +0300

    Update Solution.java

--- LeetCode/Merge-Strings-Alternately/Solution.java
+++ LeetCode/Merge-Strings-Alternately/Solution.java
@@ -1,25 +1,25 @@
-1class Solution {
-2    public String mergeAlternately(String word1, String word2) {
-3        int w1 = 0;
-4        int w2 = 0;
-5        int mergeIndex = 0;
-6
-7        char[] result = new char[word1.length() + word2.length()];
-8
-9        while(w1 < word1.length() || w2 < word2.length()){
-10
-11            if(w1 < word1.length()){
-12                result[mergeIndex] = word1.charAt(w1);
-13                mergeIndex++;
-14                w1++;
-15            }
-16            if(w2 < word2.length()){
-17                result[mergeIndex] = word2.charAt(w2);
-18                mergeIndex++;
-19                w2++;
-20            }
-21        }
-22
-23        return new String(result);
-24    }
-25}
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int w1 = 0;
+        int w2 = 0;
+        int mergeIndex = 0;
+
+        char[] result = new char[word1.length() + word2.length()];
+
+        while(w1 < word1.length() || w2 < word2.length()){
+
+            if(w1 < word1.length()){
+                result[mergeIndex] = word1.charAt(w1);
+                mergeIndex++;
+                w1++;
+            }
+            if(w2 < word2.length()){
+                result[mergeIndex] = word2.charAt(w2);
+                mergeIndex++;
+                w2++;
+            }
+        }
+
+        return new String(result);
+    }
+}
