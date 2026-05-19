8e040ee441818714b7ff239dd20fec5f074dd208
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-19 23:48:59 +0300

    Update Solution.java

--- LeetCode/Find-Pivot-Index/Solution.java
+++ LeetCode/Find-Pivot-Index/Solution.java
@@ -1,23 +1,23 @@
-1class Solution {
-2    public int pivotIndex(int[] nums) {
-3        
-4        int totalSum = 0;
-5        for(int i = 0; i < nums.length; i++){
-6            totalSum += nums[i];
-7        }
-8        
-9        int leftSum = 0;
-10        for(int i = 0; i < nums.length; i++){
-11            
-12            int rightSum = totalSum - leftSum - nums[i];  // nums[i] yi çıkardığımda eşitleniyorsa o index'in sağ ve sol toplamları eşittir.
-13
-14            if(leftSum == rightSum){
-15                return i;
-16            }
-17
-18            leftSum += nums[i];
-19        }
-20
-21        return -1;
-22    }
-23}
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        int totalSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            totalSum += nums[i];
+        }
+        
+        int leftSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            
+            int rightSum = totalSum - leftSum - nums[i];  // nums[i] yi çıkardığımda eşitleniyorsa o index'in sağ ve sol toplamları eşittir.
+
+            if(leftSum == rightSum){
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
