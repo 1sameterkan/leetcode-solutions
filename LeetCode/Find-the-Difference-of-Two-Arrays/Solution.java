681d0df49bd2649c8431e8b1d524e23562cd02f8
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-20 11:47:46 +0300

    Update Solution.java

--- LeetCode/Find-the-Difference-of-Two-Arrays/Solution.java
+++ LeetCode/Find-the-Difference-of-Two-Arrays/Solution.java
@@ -1,4 +1,34 @@
-    set1 = set(nums1)
-    set2 = set(nums2)
+    class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        
+        Set<Integer> uniqueNums1 = new HashSet<>();
+        Set<Integer> uniqueNums2 = new HashSet<>();
 
-    return [list(set1.difference(set2)), list(set2.difference(set1))]
+        for(int i = 0; i < nums1.length; i++){  // tekrarlanmayan nums1
+            uniqueNums1.add(nums1[i]);
+        }
+        for(int i = 0; i < nums2.length; i++){  // tekrarlanmayan nums2
+            uniqueNums2.add(nums2[i]);
+        }
+
+        List<Integer> answer1 = new ArrayList<>(); // nums1'de olup nums2'de olmayan elemanları tutacak liste
+        List<Integer> answer2 = new ArrayList<>(); // nums2'de olup nums1'de olmayan elemanları tutacak liste
+
+        for(int num1 : uniqueNums1){
+            if(!uniqueNums2.contains(num1)){
+                answer1.add(num1);
+            }
+        }
+        for(int num2 : uniqueNums2){
+            if(!uniqueNums1.contains(num2)){
+                answer2.add(num2);
+            }
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(answer1);
+        result.add(answer2);
+
+        return result;
+    }
+}
