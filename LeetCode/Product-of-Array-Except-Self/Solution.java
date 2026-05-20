e3cfcd41fa40e7c7944bd04eb917fead103ad2e0
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-20 23:24:16 +0300

    Update Solution.java

--- LeetCode/Product-of-Array-Except-Self/Solution.java
+++ LeetCode/Product-of-Array-Except-Self/Solution.java
@@ -1,20 +1,20 @@
-1class Solution {
-2    public int[] productExceptSelf(int[] nums) {
-3        
-4        int[] carpimlar = new int[nums.length];
-5
-6        int solCarpim = 1;
-7        for(int i = 0; i < nums.length; i++){
-8            carpimlar[i] = solCarpim;   // [1,0,0,0] --> [1,1,0,0] --> [1,1,2,0] --> [1,1,2,6]...(DÖNGÜ BİTTİ)
-9            solCarpim *= nums[i];       // 1*1=1 --> 1*2=2 --> 2*3=6 --> 6*4=24...(DÖNGÜ BİTTİ)
-10        }
-11
-12        int sagCarpim = 1;
-13        for(int j = nums.length - 1; j >= 0; j--){  // j = 3
-14            carpimlar[j] *= sagCarpim;  // [1,1,2,(6*1)] --> [1,1,(2*4),6] --> [1,(1*12),8,6] --> [(1*24),12,8,6]...(DÖNGÜ BİTTİ)
-15            sagCarpim *= nums[j];       // 1*4=4 --> 4*3=12 --> 12*2=24 --> 24*1 = 24 ...(DÖNGÜ BİTTİ)
-16        }
-17
-18        return carpimlar; 
-19    }
-20}
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int[] carpimlar = new int[nums.length];
+
+        int solCarpim = 1;
+        for(int i = 0; i < nums.length; i++){
+            carpimlar[i] = solCarpim;   // [1,0,0,0] --> [1,1,0,0] --> [1,1,2,0] --> [1,1,2,6]...(DÖNGÜ BİTTİ)
+            solCarpim *= nums[i];       // 1*1=1 --> 1*2=2 --> 2*3=6 --> 6*4=24...(DÖNGÜ BİTTİ)
+        }
+
+        int sagCarpim = 1;
+        for(int j = nums.length - 1; j >= 0; j--){  // j = 3
+            carpimlar[j] *= sagCarpim;  // [1,1,2,(6*1)] --> [1,1,(2*4),6] --> [1,(1*12),8,6] --> [(1*24),12,8,6]...(DÖNGÜ BİTTİ)
+            sagCarpim *= nums[j];       // 1*4=4 --> 4*3=12 --> 12*2=24 --> 24*1 = 24 ...(DÖNGÜ BİTTİ)
+        }
+
+        return carpimlar; 
+    }
+}
