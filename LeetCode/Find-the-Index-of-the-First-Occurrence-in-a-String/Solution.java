196c347489acb266f4030e719e0abc2a1482bc68
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-21 23:42:22 +0300

    Update Solution.java

--- LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.java
+++ LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.java
@@ -1,22 +1,22 @@
-1class Solution {
-2    public int strStr(String haystack, String needle) {
-3
-4        int n = haystack.length();
-5        int m = needle.length();
-6
-7        for (int i = 0; i <= n - m; i++) {
-8
-9            int j = 0;
-10
-11            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) { // karakter karşılaştırma
-12                j++;
-13            }
-14
-15            if (j == m) {
-16                return i;
-17            }
-18        }
-19
-20        return -1;
-21    }
-22}
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+        int n = haystack.length();
+        int m = needle.length();
+
+        for (int i = 0; i <= n - m; i++) {
+
+            int j = 0;
+
+            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) { // karakter karşılaştırma
+                j++;
+            }
+
+            if (j == m) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
