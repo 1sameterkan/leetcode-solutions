53f9d4daf95a67071407c71cda6407cf5a683195
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-21 23:41:35 +0300

    Update Solution.java

--- LeetCode/Can-Place-Flowers/Solution.java
+++ LeetCode/Can-Place-Flowers/Solution.java
@@ -1,27 +1,27 @@
-1class Solution {
-2    public boolean canPlaceFlowers(int[] flowerbed, int n) {
-3        
-4        if(n <= 0){
-5            return true;
-6        }
-7
-8        for(int i = 0; i < flowerbed.length; i++){
-9            if(flowerbed[i] == 0){  // Şu anki saksı 0 mı
-10
-11                boolean solBosMu = (i == 0 || flowerbed[i - 1] == 0);  // Başlangıçta mıyız VEYA solun boş mu 
-12
-13                boolean sagBosMu = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0);  // Sonda mıyız VEYA sağın boş mu 
-14
-15                if(solBosMu && sagBosMu){
-16                    flowerbed[i] = 1;  // Çiçeği o index'e koy
-17                    n--;  // çiçek sayısını azalt
-18
-19                    if(n == 0){
-20                        return true;
-21                    }
-22                }
-23            }
-24        }
-25        return n <= 0;  // Döngü bitmesine rağmen çiçek kalırsa sığmamıştır
-26    }
-27}
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        
+        if(n <= 0){
+            return true;
+        }
+
+        for(int i = 0; i < flowerbed.length; i++){
+            if(flowerbed[i] == 0){  // Şu anki saksı 0 mı
+
+                boolean solBosMu = (i == 0 || flowerbed[i - 1] == 0);  // Başlangıçta mıyız VEYA solun boş mu 
+
+                boolean sagBosMu = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0);  // Sonda mıyız VEYA sağın boş mu 
+
+                if(solBosMu && sagBosMu){
+                    flowerbed[i] = 1;  // Çiçeği o index'e koy
+                    n--;  // çiçek sayısını azalt
+
+                    if(n == 0){
+                        return true;
+                    }
+                }
+            }
+        }
+        return n <= 0;  // Döngü bitmesine rağmen çiçek kalırsa sığmamıştır
+    }
+}
