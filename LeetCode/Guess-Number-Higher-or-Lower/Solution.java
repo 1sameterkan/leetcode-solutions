92927cdfd8baa5a2e32eb406592fc342f05c09d2
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-22 13:37:35 +0300

    Update Solution.java

--- LeetCode/Guess-Number-Higher-or-Lower/Solution.java
+++ LeetCode/Guess-Number-Higher-or-Lower/Solution.java
@@ -1,33 +1,33 @@
-1/** 
-2 * Forward declaration of guess API.
-3 * @param  num   your guess
-4 * @return 	     -1 if num is higher than the picked number
-5 *			      1 if num is lower than the picked number
-6 *               otherwise return 0
-7 * int guess(int num);
-8 */
-9
-10public class Solution extends GuessGame {
-11    public int guessNumber(int n) {
-12        
-13        int left = 1;
-14        int right = n;
-15        
-16        while(left <= right){
-17            
-18            int mid = left + (right - left) / 2;    // mevcut tahmin ettiğimiz sayı
-19
-20            // guess(mid) --> burdan gönderdiğimiz mid değerini pick ile kıyaslar.ve o sonucu döndürür.
-21            if(guess(mid) == -1){  // pick < mid
-22                right = mid - 1;
-23            }
-24            else if(guess(mid) == 1){   // pick > mid
-25                left = mid + 1;
-26            }
-27            else{   // pick = mid
-28                return mid; 
-29            }
-30        }
-31        return -1;
-32    }
-33}
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        
+        int left = 1;
+        int right = n;
+        
+        while(left <= right){
+            
+            int mid = left + (right - left) / 2;    // mevcut tahmin ettiğimiz sayı
+
+            // guess(mid) --> burdan gönderdiğimiz mid değerini pick ile kıyaslar.ve o sonucu döndürür.
+            if(guess(mid) == -1){  // pick < mid
+                right = mid - 1;
+            }
+            else if(guess(mid) == 1){   // pick > mid
+                left = mid + 1;
+            }
+            else{   // pick = mid
+                return mid; 
+            }
+        }
+        return -1;
+    }
+}
