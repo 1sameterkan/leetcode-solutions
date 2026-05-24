db3967e41f760e0aec30f683c5bc59ee7b22c4cf
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-24 18:51:03 +0300

    Update Solution.java

--- LeetCode/Length-of-Last-Word/Solution.java
+++ LeetCode/Length-of-Last-Word/Solution.java
@@ -1,10 +1,10 @@
-1class Solution {
-2    public int lengthOfLastWord(String s) {
-3        
-4        s = s.trim();
-5        int sonBosluk = s.lastIndexOf(" ");    //Metin'deki son boşluğu bul
-6        String sonKelime = s.substring(sonBosluk + 1);  // Son kelimeyi bul.
-7        int sonKelimeUzunluk = sonKelime.length();
-8        return sonKelimeUzunluk;
-9    }
-10}
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        s = s.trim();
+        int sonBosluk = s.lastIndexOf(" ");    //Metin'deki son boşluğu bul
+        String sonKelime = s.substring(sonBosluk + 1);  // Son kelimeyi bul.
+        int sonKelimeUzunluk = sonKelime.length();
+        return sonKelimeUzunluk;
+    }
+}
