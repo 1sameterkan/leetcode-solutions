bb597362018e64a653b9bfd5f5af1d1c575e28d2
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-24 18:54:33 +0300

    Fix formatting issues in Solution.java

--- LeetCode/Valid-Parentheses/Solution.java
+++ LeetCode/Valid-Parentheses/Solution.java
@@ -1,33 +1,33 @@
-1class Solution {
-2    public boolean isValid(String s) {
-3        Stack<Character> stack = new Stack<>();
-4
-5        for(char c : s.toCharArray()){
-6            if(c == '(' || c == '{' || c == '['){
-7                stack.push(c);
-8            }
-9            else{
-10
-11                if(stack.isEmpty()){
-12                    return false;
-13                }
-14                
-15                char top = stack.pop();
-16
-17                if(c == ')' && top != '(') {
-18                    return false;
-19                }
-20                
-21                if(c == ']' && top != '[') {
-22                    return false;
-23                }
-24                
-25                if(c == '}' && top != '{') {
-26                    return false;
-27                }
-28
-29            }
-30        }
-31        return stack.isEmpty();
-32    }
-33}
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            else{
+
+                if(stack.isEmpty()){
+                    return false;
+                }
+                
+                char top = stack.pop();
+
+                if(c == ')' && top != '(') {
+                    return false;
+                }
+                
+                if(c == ']' && top != '[') {
+                    return false;
+                }
+                
+                if(c == '}' && top != '{') {
+                    return false;
+                }
+
+            }
+        }
+        return stack.isEmpty();
+    }
+}
