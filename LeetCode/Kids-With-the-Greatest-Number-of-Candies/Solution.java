56620f710c4dbf89838093f2061b9b3333dded3b
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-24 18:52:31 +0300

    Update Solution.java

--- LeetCode/Kids-With-the-Greatest-Number-of-Candies/Solution.java
+++ LeetCode/Kids-With-the-Greatest-Number-of-Candies/Solution.java
@@ -1,23 +1,23 @@
-1class Solution {
-2    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-3        
-4        List<Boolean> result = new ArrayList<>();
-5
-6        int max = candies[0];
-7        for(int i = 1; i < candies.length; i++){
-8            if(candies[i] > max){
-9                max = candies[i];
-10            }
-11        }
-12
-13        for(int i = 0; i < candies.length; i++){
-14            if(candies[i] + extraCandies >= max){
-15                result.add(true);
-16            }
-17            else{
-18                result.add(false);
-19            }
-20        }
-21        return result;
-22    }
-23}
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        List<Boolean> result = new ArrayList<>();
+
+        int max = candies[0];
+        for(int i = 1; i < candies.length; i++){
+            if(candies[i] > max){
+                max = candies[i];
+            }
+        }
+
+        for(int i = 0; i < candies.length; i++){
+            if(candies[i] + extraCandies >= max){
+                result.add(true);
+            }
+            else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
