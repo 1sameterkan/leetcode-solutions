0121951fdc99475411e18f193fcc4c24eb7509b8
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-24 18:53:35 +0300

    Update Solution.java

--- LeetCode/Palindrome-Number/Solution.java
+++ LeetCode/Palindrome-Number/Solution.java
@@ -1,18 +1,18 @@
-1class Solution {
-2    public boolean isPalindrome(int x) {
-3        if (x < 0) {
-4            return false;
-5        }
-6
-7        int n = x;
-8        int revnum = 0;
-9
-10        while (n > 0) {
-11            int d = n % 10;
-12            revnum = revnum * 10 + d;
-13            n = n / 10;
-14        }
-15
-16        return revnum == x;
-17    }
-18}
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int n = x;
+        int revnum = 0;
+
+        while (n > 0) {
+            int d = n % 10;
+            revnum = revnum * 10 + d;
+            n = n / 10;
+        }
+
+        return revnum == x;
+    }
+}
