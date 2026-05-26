a1997db3b0e3113362e2a44095c608e45ecca934
Samet <91949614+1sameterkan@users.noreply.github.com>
2026-05-26 23:54:46 +0300

    Update Solution.java

--- LeetCode/Two-Sum/Solution.java
+++ LeetCode/Two-Sum/Solution.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
 
         Map<Integer, Integer> numMap = new HashMap<>();
-
         for(int i  = 0; i < nums.length; i++) {
             int complement = target - nums[i];
 
